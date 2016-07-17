c2197e0c67bb7b502703d0e013015459161a5586
Siddharth <siddharthagout@gmail.com>
2016-07-17 20:48:29 +0530

    Add files via upload
    Added config file

---
+++ config.py
@@ -0,0 +1,6 @@
+
+base_url = "http://cc.porsche.com/icc_euro/ccCall.do?rt=1468485520&screen=1920x1080&userID=CC&lang=cc&PARAM=" \
+           "parameter_internet_cc&ORDERTYPE=991520&customID=cc&MODELYEAR=2017&hookURL=http%3a%2f%2fwww.porsche." \
+           "com%2finternational%2fmodelstart%2f"
+
+logs = "run.log"
