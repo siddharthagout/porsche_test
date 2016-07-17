681fd76134c1d01f06de6f4ef5800b0805945fdc
Siddharth <siddharthagout@gmail.com>
2016-07-17 21:31:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 
+#base url to use for testing
 base_url = "http://cc.porsche.com/icc_euro/ccCall.do?rt=1468485520&screen=1920x1080&userID=CC&lang=cc&PARAM=" \
            "parameter_internet_cc&ORDERTYPE=991520&customID=cc&MODELYEAR=2017&hookURL=http%3a%2f%2fwww.porsche." \
            "com%2finternational%2fmodelstart%2f"
+           
 
+#Chrome driver location           
+chromedriver = "C:\Users\Swiggy\Documents\chromedriver_win32\chromedriver"
+
+#log file name
 logs = "run.log"
