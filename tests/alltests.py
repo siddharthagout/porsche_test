83cd9e8dc4592d71b34c9d2871cbe2a5d16e6e4b
Siddharth <siddharthagout@gmail.com>
2016-07-17 20:50:09 +0530

    Create alltests.py
    added two testcases to repo

---
+++ tests/alltests.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import os
+
+from config import *
+
+
+logfile = open(logs, 'w')
+
+def getTitleName():
+    """"This test gets title from page"""
+    driver = webdriver.Firefox()
+    driver.get(base_url)
+    driver.maximize_window()
+    assert "Porsche Car Configurator" in driver.title
+    print "method : {0} - PASS".format(getTitleName.__name__)
+    logfile.write("method : {0} - PASS".format(getTitleName.__name__))
+    logfile.write("\n")
+    driver.close()
+
+def compatibility_mode_chrome():
+    """"Here we are checking compatibility of website with chrome"""
+    chromedriver = "C:\Users\Swiggy\Documents\chromedriver_win32\chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(chromedriver)
+    driver.get(base_url)
+    driver.maximize_window()
+    assert "Porsche Car Configurator" in driver.title
+    print "method : {0} - PASS".format(compatibility_mode_chrome.__name__)
+    logfile.write("method : {0} - PASS".format(compatibility_mode_chrome.__name__))
+    logfile.write("\n")
+    driver.close()
+
+
+if __name__ == '__main__':
+    funcs = [getTitleName,compatibility_mode_chrome]
+    for func in funcs:
+        try:
+            func()
+        except:
+            print "this method is failed : {0}".format(func)
+    #getTitleName()
+    #compatibility_mode_chrome()
